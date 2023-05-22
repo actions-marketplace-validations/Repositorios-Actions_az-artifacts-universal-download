9bd219bcecf3a0894ff5791056a90981458ecff3
Anthony Moreno <anthony.moreno.cisnero@gmail.com>
2023-05-22 05:25:48 -0400

    add variables para usar maven y java

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,5 +1,15 @@
 #!/bin/sh -l
 
+#Variables para JAVA
+JAVA_HOME='/opt/jdk-13.0.1'
+PATH="$JAVA_HOME/bin:$PATH"
+export PATH
+
+#Variables para MAVEN
+M2_HOME='/opt/apache-maven-3.6.3'
+PATH="$M2_HOME/bin:$PATH"
+export PATH
+
 az login -u $1 -p $2
 az artifacts universal download --organization "$3" --project "$4" --scope project --feed "$5" --name "$6" --version $7 --path $8
 $9
