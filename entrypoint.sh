a2121bba44f9b5239bd5a6d53e7673c73f5b10e0
Anthony Moreno <anthony.moreno.cisnero@gmail.com>
2023-05-22 04:38:26 -0400

    add la ejecución el comando adicional

--- entrypoint.sh
+++ entrypoint.sh
@@ -2,5 +2,6 @@
 
 az login -u $1 -p $2
 az artifacts universal download --organization "$3" --project "$4" --scope project --feed "$5" --name "$6" --version $7 --path $8
+$9
 time=$(date)
 echo "time=$time" >> $GITHUB_OUTPUT
