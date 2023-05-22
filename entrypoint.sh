38c80566a049f0e193e6ed1dd4e8b692e17aaddf
Anthony Moreno <anthony.moreno.cisnero@gmail.com>
2023-05-22 06:27:09 -0400

    add ls -l para visualizar los archivos

--- entrypoint.sh
+++ entrypoint.sh
@@ -19,10 +19,12 @@ az login -u $1 -p $2
 echo "###             CREATE DIRECTORY $6             ###"
 echo "###################################################"
 mkdir $6
+ls -l
 
 echo "###      AZ ARTIFACT UNIVERSAL DOWNLOAD         ###"
 echo "###################################################"
 cd $6 && az artifacts universal download --organization "$3" --project "$4" --scope project --feed "$5" --name "$6" --version $7 --path $8
+ls -l
 
 echo "###               OPTIONAL COMMAND              ###"
 echo "###################################################"
