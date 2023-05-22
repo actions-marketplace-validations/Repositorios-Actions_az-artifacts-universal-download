6ebfde2855b677f501aff1c11e31fa0a120c140d
Anthony Moreno <anthony.moreno.cisnero@gmail.com>
2023-05-22 04:24:35 -0400

    add script para descarga de artefacto

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,5 +1,6 @@
 #!/bin/sh -l
 
 az login -u $1 -p $2
+az artifacts universal download --organization "$3" --project "$4" --scope project --feed "$5" --name "$6" --version $7 --path $8
 time=$(date)
 echo "time=$time" >> $GITHUB_OUTPUT
