e95b5e7a00c49608a788506781282f47fb0cb9ec
Anthony Moreno <anthony.moreno.cisnero@gmail.com>
2023-05-22 04:50:36 -0400

    Se añade un az logout en el entrypoint

--- entrypoint.sh
+++ entrypoint.sh
@@ -3,5 +3,6 @@
 az login -u $1 -p $2
 az artifacts universal download --organization "$3" --project "$4" --scope project --feed "$5" --name "$6" --version $7 --path $8
 $9
+az logout
 time=$(date)
 echo "time=$time" >> $GITHUB_OUTPUT
