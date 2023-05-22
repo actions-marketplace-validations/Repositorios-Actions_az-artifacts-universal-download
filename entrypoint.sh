5e6bcd6cd14572c56c60a3c2e3a31d612126bb29
Anthony Moreno <anthony.moreno.cisnero@gmail.com>
2023-05-22 05:52:30 -0400

    add step by step para en entrypoint

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,18 +1,32 @@
 #!/bin/sh -l
 
-#Variables para JAVA
+echo "###                EXPORT JAVA_HOME             ###"
+echo "###################################################"
 JAVA_HOME='/opt/jdk-13.0.1'
 PATH="$JAVA_HOME/bin:$PATH"
 export PATH
 
-#Variables para MAVEN
+echo "###                EXPORT M2_HOME               ###"
+echo "###################################################"
 M2_HOME='/opt/apache-maven-3.6.3'
 PATH="$M2_HOME/bin:$PATH"
 export PATH
 
+echo "###                  AZ LOGIN                   ###"
+echo "###################################################"
 az login -u $1 -p $2
+
+echo "###      AZ ARTIFACT UNIVERSAL DOWNLOAD         ###"
+echo "###################################################"
 az artifacts universal download --organization "$3" --project "$4" --scope project --feed "$5" --name "$6" --version $7 --path $8
+
+echo "###               OPTIONAL COMMAND              ###"
+echo "###################################################"
 $9
+
+echo "###                   AZ LOGOUT                  ###"
+echo "###################################################"
 az logout
+
 time=$(date)
 echo "time=$time" >> $GITHUB_OUTPUT
