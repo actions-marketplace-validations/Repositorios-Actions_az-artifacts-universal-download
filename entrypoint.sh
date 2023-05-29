7ee2b10a6f3ec25ec7a455e09e0256b0a7f191f2
Anthony Moreno <anthony.moreno.cisnero@gmail.com>
2023-05-29 13:08:05 -0400

    Se elmina el seteo de las variables java y mvn

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,17 +1,5 @@
 #!/bin/sh -l
 
-echo "###                EXPORT JAVA_HOME             ###"
-echo "###################################################"
-JAVA_HOME='/opt/jdk-13.0.1'
-PATH="$JAVA_HOME/bin:$PATH"
-export PATH
-
-echo "###                EXPORT M2_HOME               ###"
-echo "###################################################"
-M2_HOME='/opt/apache-maven-3.6.3'
-PATH="$M2_HOME/bin:$PATH"
-export PATH
-
 echo "###                  AZ LOGIN                   ###"
 echo "###################################################"
 az login -u $1 -p $2
@@ -23,12 +11,8 @@ ls -l
 
 echo "###      AZ ARTIFACT UNIVERSAL DOWNLOAD         ###"
 echo "###################################################"
-cd $6 && az artifacts universal download --organization "$3" --project "$4" --scope project --feed "$5" --name "$6" --version $7 --path $8
+cd $6 && az artifacts universal download --organization "$3" --project "$4" --scope "$5" --feed "$6" --name "$7" --version $8 --path $9
 ls -l
-
-echo "###               OPTIONAL COMMAND              ###"
-echo "###################################################"
-$9
 
 echo "###                   AZ LOGOUT                  ###"
 echo "###################################################"
